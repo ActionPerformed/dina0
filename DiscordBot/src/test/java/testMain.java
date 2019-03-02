f196ecfe1834a0a078228433c1dfaa385d129555
ActionPerformed <Juan@Juan-PC>
2019-03-02 18:29:53 +0100

    Commit inicial

---
+++ DiscordBot/src/test/java/testMain.java
@@ -0,0 +1,9 @@
+public class testMain {
+	public static void main(String[] args){
+		String[] res1 = "hola-adios".split("-");
+		String[] res2 = "holaadios".split("-");
+		String[] res3 = "".split("-");
+		String[] res4 = "hola-adios".split("-");
+		System.out.println("hola");
+	}
+}
